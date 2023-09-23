602db2c05489debb5ddd0befc6bf471b7d25143f
BrianLupu93 <brianlupu@gmail.com>
2023-09-23 10:00:17 +0200

    schedule day done

--- cron-jobs/scheduledJobs.js
+++ cron-jobs/scheduledJobs.js
@@ -5,7 +5,7 @@ const Subscription = require('../models/subscriptionModel');
 
 exports.checkDoneTrainings = () => {
   //   Every 20 minutes  */20 * * * *
-  schedule.scheduleJob('*/1 * * * *', async () => {
+  schedule.scheduleJob('*/20 6-23 * * *', async () => {
     const today = dayjs().format('DD/MM/YYYY');
     const hour = dayjs().hour();
     const minute = dayjs().minute();
